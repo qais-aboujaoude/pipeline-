00c0e025275a0127a2f02437801607d4f7bcb1ec
Qais Issa Aboujaoude <q.i.aboujaoude@gmail.com>
2018-03-18 21:28:09 +0200

    Refactor into a exportable object

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-process.env.AWS_SDK_LOAD_CONFIG = true
+const codepipeline = require('./src/codepipeline-sdk')
 
-const AWS = require('aws-sdk'),
-      CodePipeline = new AWS.CodePipeline(),
-      express = require('express')
-
-const app = express()
-
-app.get('/', (req, res) => {
-  CodePipeline.getPipelineState({name: 'haulo-api-demo-pipeline'}, (err, data) => {
-    err ? console.log(err, err.stack) : res.send(data)
-  })  
-}) 
-
-
-app.listen(3000, () => console.log('Server running on port 3000'))
-
+codepipeline.getListfPipelineNames()
+  .then(r => console.log(r))
+  .catch(err => console.log(err))
 
--- src/codepipeline-sdk.js
+++ src/codepipeline-sdk.js
@@ -3,17 +3,29 @@ process.env.AWS_SDK_LOAD_CONFIG = true
 const AWS = require('aws-sdk'),
       CodePipeline = new AWS.CodePipeline()
 
+module.exports = {
 
-
-
-const getListfPipelineNames = () => {
-  return new Promise((resolve, reject) => {
-    CodePipeline.listPipelines({}, (err, data) => {
-      err ? reject(err, err.stack) : resolve(data.pipelines.map(p => p.name))
+  /**
+   * @method getListfPipelineNames
+   * Calls the CodePipeline SDK and returns all pipelines names associated
+   * with the user 
+   */
+  getListfPipelineNames: () => {
+    return new Promise((resolve, reject) => {
+      CodePipeline.listPipelines({}, (err, data) => {
+        err ? reject(err, err.stack) : resolve(data)
+        // err ? reject(err, err.stack) : resolve(data.pipelines.map(p => p.name))
+      })
     })
-  })
+  },
+
+
 }
 
+
+// module.exports = getListfPipelineNames
+
+/*
 CodePipeline.getPipeline({name: 'haulo-api-demo-pipeline'}, (err, data) => {
   err ? console.log(err, err.stack) : console.log(data)
 })
@@ -25,3 +37,9 @@ CodePipeline.getPipelineState({name: 'haulo-api-demo-pipeline'}, (err, data) => 
 CodePipeline.listPipelineExecutions({pipelineName: 'haulo-api-demo-pipeline'}, (err, data) => {
    err ? console.log(err, err.stack) : console.log(data)
 })
+
+CodePipeline.getPipelineState({name: 'haulo-api-demo-pipeline'}, (err, data) => {
+  err ? console.log(err, err.stack) : res.send(data)
+})  
+*/
+
