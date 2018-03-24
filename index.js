cc3dc362b58d3c51c068e6387d3462e7d06b25f0
Qais Issa Aboujaoude <q.i.aboujaoude@gmail.com>
2018-03-24 19:20:56 +0200

    Add cli-table2 and add basic table for list of pipes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Commander will go here
+const pipeline = require('./src/codepipeline-sdk')
+var Table = require('cli-table2');
+var table = new Table({
+  head: ['List of Pipelines']
+  // colWidths: [40]
+});
+
+pipeline.getListfPipelineNames()
+  .then(r => {
+    r.forEach(e => table.push([e]))
+    console.log(table.toString())
+  })
+  .catch(err => console.log(err))
+
+
+
+// table is an Array, so you can `push`, `unshift`, `splice` and friends
+// table.push(
+//   ['First value']
+// );
