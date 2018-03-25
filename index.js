99e32d75b032da930d72fe5ed4f766156ab3dd8c
Qais Issa Aboujaoude <q.i.aboujaoude@gmail.com>
2018-03-25 19:24:03 +0300

    Add Name Option to be used by methods that require name

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const display = require('./src/formatted-tables')
 program
   .version('0.1.0')
   .option('-l, --list', 'list pipelines')
-  .option('-n, --names', 'List Names')
+  .option('-d, --names', 'List Names')
   .option('-p, --pipeline', 'List Names')
+  .option('-n, --name [name]', 'name of the pipeline')
   .parse(process.argv)
 
 if(program.names) display.displayListofNames() 
 if(program.list) display.displayListofPipelines()
-if(program.pipeline) display.pipelineInformation()
+if(program.pipeline) display.pipelineInformation(program.name)
 
 
